eadff1a1d074e8bd25b3572f69ec06e9a8ef43a7
Fahim Ahmadi <48436831+fahimahmadi1985@users.noreply.github.com>
2021-05-07 10:01:31 +0200

    initialize the project and add readme file

---
+++ script.js
@@ -0,0 +1,19 @@
+
+// Project Name: MEMORY GAME 1.1
+
+/* 
+
+
+Product details:
+    1- create table 4x4
+    2- set an image randomly to every cell
+    3- onclick on the cells the images should flip back
+    4- after selecting the second image, they should be compared and 
+    if they are matched keep them visible otherwise flip them back after 2 second
+    5- on every correct attempt, the use get 10points, and on failures 10 point deducted
+    6- user has 90sec to finish the game otherwise the game we be restarted
+    7- if user finish the game successfully on time then 
+    we should display a modal with greeting message and points and time 
+    8- we should have a restart button to be able to restart the game at anytime
+
+*/
